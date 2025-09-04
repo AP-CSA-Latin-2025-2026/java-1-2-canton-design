c7aa35f09fdc38658b29df08a1f8dfdabef0c7f5
canton-design <canton@lsoc.org>
2025-09-04 17:34:09 +0000

    54b

---
+++ src/org/lsoc/HW54b.java
@@ -0,0 +1,14 @@
+public class HW54b {
+  public static void main(String[] args){
+  int number1 = 475;
+  int number2 = 821;
+  int number3 = 369;
+  int number4 = 562;
+
+  int sumnumbers = number1 + number2 + number3 + number4;
+  int avgnumbers = sumnumbers / 5;
+
+
+
+  }
+}
